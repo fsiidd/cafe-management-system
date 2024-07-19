36a2a13ad03aa6c8407c20d7bb621f877e54e34b
Faryal Siddiqui <faryalsiddiqui666@gmail.com>
2024-07-19 00:03:13 -0400

    implemented JWT tokens with apis

--- src/main/java/com/inn/cafe/JWT/JWTFilter.java
+++ src/main/java/com/inn/cafe/JWT/JWTFilter.java
@@ -2,6 +2,11 @@ package com.inn.cafe.JWT;
 
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,17 +30,48 @@ public class JWTFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-
-        if(httpServletRequest.getServletPath().matches("/user/login|/user/forgetPassword|/user/signup")){
-            filterChain.doFilter(httpServletRequest,httpServletResponse);
-        } else {
+        // if the api requests are for login, signup, or forgotPassword, continue, otherwise required authentication
+        if (httpServletRequest.getServletPath().matches("/user/login|/user/signup|/user/forgotPassword")) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        }
+        else {
             String authorizationHeader = httpServletRequest.getHeader("Authorization");
             String token = null;
 
-            if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            // authorization is successful
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
                 token = authorizationHeader.substring(7);
                 userName = jwtUtil.extractUsername(token);
+                claims = jwtUtil.extractAllClaims(token);
             }
+
+            // user is valid
+            if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = service.loadUserByUsername(userName);
+                if (jwtUtil.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(
+                            // converts HttpServletRequest into an instance of the WebAuthenticationDetails class
+                            // HttpServletRequest = parsed raw HTTP data
+                            new WebAuthenticationDetailsSource().buildDetails(httpServletRequest)
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+            // continue filtration
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
         }
+    }
+    public boolean isAdmin() {
+        return "admin".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    public boolean isUser() {
+        return "user".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    public String getCurrentUser() {
+        return userName;
     }
 }
--- src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.inn.cafe.serviceImpl;
 
+import com.inn.cafe.JWT.CustomerUsersDetailsService;
+import com.inn.cafe.JWT.JWTFilter;
+import com.inn.cafe.JWT.JWTUtil;
 import com.inn.cafe.constants.CafeConstants;
 import com.inn.cafe.dao.UserDao;
 import com.inn.cafe.modals.User;
@@ -7,10 +10,15 @@ import com.inn.cafe.service.UserService;
 import com.inn.cafe.utils.CafeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
@@ -20,6 +28,17 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserDao userDao;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    CustomerUsersDetailsService customerUsersDetailsService;
+
+    @Autowired
+    JWTUtil jwtUtil;
+
+
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
         //when we hit the api endpoint, this logs all the data that is available in the requestMap,
@@ -38,15 +57,15 @@ public class UserServiceImpl implements UserService {
                 return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
             }
         } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+            ex.printStackTrace();
         }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 
     private boolean validateSignUpMap(Map<String, String> requestMap) {
         if (requestMap.containsKey("name") && requestMap.containsKey("contactNumber")
-                && requestMap.containsKey("email") &&  requestMap.containsKey("password")) {
+                && requestMap.containsKey("email") && requestMap.containsKey("password")) {
             return true;
         }
         return false;
@@ -57,9 +76,35 @@ public class UserServiceImpl implements UserService {
         user.setName(requestMap.get("name"));
         user.setContactNumber(requestMap.get("contactNumber"));
         user.setEmail(requestMap.get("email"));
+        user.setPassword(requestMap.get("password"));
         user.setStatus("false");
         user.setRole("user");
         return user;
     }
 
+    @Override
+    public ResponseEntity<String> login(Map<String, String> requestMap) {
+        log.info("Inside login");
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(requestMap.get("email")
+                            , requestMap.get("password"))
+            );
+            if (auth.isAuthenticated()) {
+                if (customerUsersDetailsService.getUserDetail().getStatus().equalsIgnoreCase("true")) {
+                    return new ResponseEntity<String>("{\"token\":\"" +
+                            jwtUtil.generateToken(customerUsersDetailsService.getUserDetail().getEmail(),
+                                    customerUsersDetailsService.getUserDetail().getRole()) + "\"}",
+                            HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<String>("{\"message\":\"" + "Wait for admin approval." + "\"}",
+                            HttpStatus.BAD_REQUEST);
+                }
+            }
+        } catch (Exception ex) {
+            log.error("{}", ex);
+        }
+        return new ResponseEntity<String>("{\"message\":\"" + "Bad Credentials." + "\"}",
+                HttpStatus.BAD_REQUEST);
+    }
 }
